3410137de1d4850d38339a38fe0e767e363370fc
VinodSys <VinodSys@DESKTOP-ORVPEVU>
2020-11-09 15:46:06 +0530

    commit from Vinod Nagda

--- src/com/names/Names.java
+++ src/com/names/Names.java
@@ -4,7 +4,8 @@ public class Names {
 
 	public static void main(String[] args) {
 	System.out.println("This is mahesh karad"); 
-	System.out.println("This is Vinod Nagda");
+	System.out.println("This is Vinod Nagda ");
+	
 	}
 
 }
