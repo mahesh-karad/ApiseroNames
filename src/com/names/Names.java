d51a4f7dfd968116d558276a8f6d4c9f462060da
Dell <Dell@DELL-02>
2020-11-09 14:27:44 +0530

    This is a commit from Aakaash

--- src/com/names/Names.java
+++ src/com/names/Names.java
@@ -4,7 +4,7 @@ public class Names {
 
 	public static void main(String[] args) {
 	System.out.println("This is mahesh karad"); 
-	System.out.println("this is kamini kumari");
+	System.out.println("this is Aakaash KS");
 	}
 
 }
