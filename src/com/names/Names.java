84f0d3d16fdd90aeb3e0cb84eb5e7d2bce678ec9
DELL <arundhati.sarkar@apisero.com>
2020-11-09 15:45:44 +0530

    Second Commit

--- src/com/names/Names.java
+++ src/com/names/Names.java
@@ -5,7 +5,7 @@ public class Names {
 	public static void main(String[] args) {
 	System.out.println("This is mahesh karad"); 
 	System.out.println("this is Aakaash KS");
-	System.out.println("this is Arundhati Sarkar");
+	System.out.println("This is Arundhati Sarkar");
 	}
 
 }
